8b227a6dc4741b6ab895474f88889e5664c45ef4
Stephen <stephenmissah@outlook.com>
2026-05-03 20:43:16 +0100

    create custom hook calling thunks if idle

---
+++ src/Hooks/Hooks.tsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import type { AppDispatch, RootState } from "../store";
+
+export function useFetchOnIdle<T>(
+  selector: (state: RootState) => T,
+  thunk: any,
+  errorMessage: string
+) {
+  const dispatch = useDispatch<AppDispatch>();
+  const status = useSelector(selector);
+
+  useEffect(() => {
+    if (status === "idle") {
+      dispatch(thunk())
+        .unwrap()
+        .catch((err: unknown) => {
+          console.error(errorMessage, err);
+        });
+    }
+  }, [status, dispatch, thunk, errorMessage]);
+}
