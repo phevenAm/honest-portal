79a64d760b6b4dc4e287bada673207be97e6ccff
Stephen <stephenmissah@outlook.com>
2026-06-11 19:52:56 +0100

    introduce path aliases

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,26 @@
-import { defineConfig } from "vite";
+import {
+  defineConfig
+} from "vite";
 import react from "@vitejs/plugin-react";
-
+import {
+  fileURLToPath,
+  URL
+} from 'url'
 export default defineConfig({
   plugins: [react()],
+
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+      '@context': fileURLToPath(new URL('./src/context', import.meta.url)),
+      '@store': fileURLToPath(new URL('./src/store', import.meta.url)),
+      '@models': fileURLToPath(new URL('./src/models', import.meta.url)),
+      '@styles': fileURLToPath(new URL('./src/styles', import.meta.url)),
+      '@lib': fileURLToPath(new URL('./src/lib', import.meta.url)),
+    }
+  },
 
   css: {
     devSourcemap: true,
