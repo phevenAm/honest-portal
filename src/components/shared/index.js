99aab548fd5a7ebb7fa2a8bc11f58ef184101bf8
Stephen <stephenmissah@outlook.com>
2026-07-12 23:27:05 +0100

    export all components from one place

---
+++ src/components/shared/index.js
@@ -0,0 +1,33 @@
+export { default as Avatar } from "./Avatar/Avatar";
+export { default as Button } from "./Button/Button";
+export { default as Card } from "./Card/Card";
+export { default as DemoBanner } from "./DemoBanner/DemoBanner";
+export { default as Footer } from "./Footer/Footer";
+export {
+  ArticleIcon,
+  BookIcon,
+  CheckIcon,
+  ChevronDown,
+  ClipboardIcon,
+  CloseIcon,
+  DocumentIcon,
+  LinkIcon,
+  LogoIcon,
+  MailIcon,
+  MenuIcon,
+  MoonIcon,
+  PlusIcon,
+  Settingsicon,
+  SunIcon,
+  UsersIcon,
+  VideoIcon,
+} from "./Icons/Icons";
+export { default as Modal } from "./Modal/Modal";
+export { default as Navbar } from "./Navbar/Navbar";
+export { default as ProgressChart } from "./ProgressChart/ProgressChart";
+export { default as ProtectedRoute } from "./ProtectedRoute/ProtectedRoute";
+export { default as Search } from "./Search/Search";
+export { default as SkipToMain } from "./SkipToMain/SkipToMain";
+export { default as SplitButton } from "./SplitButton/SplitButton";
+export { default as UploadAndDisplayImage } from "./UploadAndDisplayImage/UploadAndDisplayImage";
+export { default as WIP } from "./WIP/WIP";
